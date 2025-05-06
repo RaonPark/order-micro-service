e0a870c918291d82372f886d217439348bf1db0c
raonpark <sumin9278@gmail.com>
2025-05-06 16:38:45 +0900

    [feat] mongodb k8s

---
+++ kubernetes/mongodb/mongo_replset.sh
@@ -0,0 +1,7 @@
+rs.initiate({
+  _id: "rs0",
+  members: [
+    { _id: 0, host: "mongodb-0.mongodb.default.svc.cluster.local:27017"},
+    { _id: 1, host: "mongodb-1.mongodb.default.svc.cluster.local:27017"}
+  ]
+})
